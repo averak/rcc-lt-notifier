7c54cf007d16245f48ba93891f1b858528ebc5ac
averak <abe12@mccc.jp>
2021-04-23 23:52:19 +0900

    [chore] webpackの設定ファイルを修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,25 @@
 const path = require("path");
 const GasPlugin = require("gas-webpack-plugin");
-const Es3ifyPlugin = require("es3ify-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  devtool: "inline-source-map",
-  context: __dirname,
   entry: {
-    main: path.resolve(__dirname, "src", "index.ts"),
+    main: path.join(__dirname, "src", "index.ts"),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
+    path: path.join(__dirname, "dist"),
   },
   module: {
     rules: [
       {
-        test: /\.[tj]s$/,
-        loader: "babel-loader",
+        test: /\.ts$/,
+        use: "ts-loader",
       },
     ],
   },
-  plugins: [new GasPlugin(), new Es3ifyPlugin()],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new GasPlugin()],
 };
